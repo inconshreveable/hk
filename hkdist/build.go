5a6965843904e2b991d25360261954ccc7e0530e
Blake Gentry <blakesgentry@gmail.com>
2013-09-17 17:10:56 -0700

    working cross-compiled builds

--- hkdist/build.go
+++ hkdist/build.go
@@ -14,15 +14,8 @@ import (
 	"net/url"
 	"os"
 	"os/exec"
-	"runtime"
 	"strings"
 	"time"
-)
-
-const (
-	repoURL   = "https://github.com/kr/hk.git"
-	buildPlat = runtime.GOOS + "-" + runtime.GOARCH
-	dir       = "hk"
 )
 
 func mustHaveEnv(name string) {
@@ -31,35 +24,64 @@ func mustHaveEnv(name string) {
 	}
 }
 
-func build() {
-	mustHaveEnv("S3DISTURL")
-	mustHaveEnv("S3_ACCESS_KEY")
-	mustHaveEnv("S3_SECRET_KEY")
-	mustHaveEnv("BUILDBRANCH")
-	mustHaveEnv("BUILDNAME")
-	mustHaveEnv("DISTURL")
-	if err := os.RemoveAll(dir); err != nil {
-		log.Fatal(err)
+type Build struct {
+	Repo   string
+	Dir    string
+	Branch string
+	Name   string
+	Arch   string
+	OS     string
+	ver    string
+}
+
+func (b *Build) Platform() string {
+	return b.OS + "-" + b.Arch
+}
+
+func (b *Build) Run() error {
+	if err := os.RemoveAll(b.Dir); err != nil {
+		return err
 	}
-	mustCmd("git", "clone", "-b", branch, repoURL, dir)
-	if err := os.Chdir(dir); err != nil {
-		log.Fatal(err)
-	}
-	ver := mustBuild()
-	body, err := os.Open(buildName)
-	if err != nil {
-		log.Fatal(err)
+	if _, err := cmd("git", "clone", "-b", b.Branch, b.Repo, b.Dir); err != nil {
+		return fmt.Errorf("clone failure: ", err)
 	}
 
-	sha256 := mustSha256(body)
+	if err := os.Chdir(b.Dir); err != nil {
+		return err
+	}
+	err := b.cloneAndBuild()
+	if err != nil {
+		return err
+	}
+	body, err := os.Open(b.Name)
+	if err != nil {
+		return fmt.Errorf("open failure: %s", err)
+	}
+
+	h := sha256.New()
+	if _, err := io.Copy(h, body); err != nil {
+		return err
+	}
+	shasum := h.Sum(nil)
+
 	_, err = body.Seek(int64(0), 0)
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 
-	mustUpload(body, ver)
-	mustRegister(ver, sha256)
-	mustSetCur(ver)
+	if err = b.upload(body); err != nil {
+		return fmt.Errorf("upload failure: %s", err)
+	}
+	if err = b.register(shasum); err != nil {
+		return fmt.Errorf("registration failure: %s", err)
+	}
+	if err = b.setCurVersion(); err != nil {
+		return fmt.Errorf("release failure: %s", err)
+	}
+	if err := os.Chdir(".."); err != nil {
+		return err
+	}
+	return nil
 }
 
 const relverGo = `
@@ -69,72 +91,62 @@ package main
 const Version = %q
 `
 
-func mustBuild() (ver string) {
-	tag := string(bytes.TrimSpace(mustCmd("git", "describe")))
-	if tag[0] != 'v' {
-		log.Fatal("bad tag name: ", tag)
+func (b *Build) cloneAndBuild() (err error) {
+	tagb, err := cmd("git", "describe")
+	if err != nil {
+		return fmt.Errorf("error listing tags: %s", err)
 	}
-	ver = tag[1:]
+	tag := string(bytes.TrimSpace(tagb))
+	if tag[0] != 'v' {
+		return fmt.Errorf("bad tag name: %s", tag)
+	}
+	ver := tag[1:]
 	if strings.IndexFunc(ver, badVersionRune) >= 0 {
-		log.Fatal("bad tag name: ", tag)
+		return fmt.Errorf("bad tag name: %s", tag)
 	}
 	// TODO(kr): verify signature
-	url := distURL + buildName + "-" + ver + "-" + buildPlat + ".json"
+	url := distURL + b.Name + "-" + ver + "-" + b.OS + "-" + b.Arch + ".json"
 	if _, err := fetchBytes(url); err == nil {
-		log.Fatal("already built: ", ver)
+		return fmt.Errorf("already built: %s", ver)
 	}
 
 	f, err := os.Create("relver.go")
 	if err != nil {
-		log.Fatal(err)
+		return fmt.Errorf("error writing relver.go: %s", err)
 	}
 	_, err = fmt.Fprintf(f, relverGo, ver)
 	if err != nil {
-		log.Fatal(err)
+		return fmt.Errorf("error writing relver.go: %s", err)
 	}
-	log.Println("go build -tags release -o " + buildName)
-	cmd := exec.Command("go", "build", "-tags", "release", "-o", buildName)
+	log.Printf("GOOS=%s GOARCH=%s go build -tags release -o %s\n", b.OS, b.Arch, b.Name)
+	cmd := exec.Command("go", "build", "-tags", "release", "-o", b.Name)
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
+	cmd.Env = append([]string{fmt.Sprintf("GOOS=%s", b.OS), fmt.Sprintf("GOARCH=%s", b.Arch)}, os.Environ()...)
 	err = cmd.Run()
 	if err != nil {
-		log.Fatal("go build -tags release: ", err)
+		return fmt.Errorf("go build -tags release: ", err)
 	}
-	return ver
+	b.ver = ver
+	return nil
 }
 
-func mustSha256(r io.Reader) ([]byte) {
-	h := sha256.New()
-	if _, err := io.Copy(h, r); err != nil {
-		log.Fatal(err)
-	}
-	return h.Sum(nil)
-}
-
-func mustUpload(r io.Reader, ver string) {
+func (b *Build) upload(r io.Reader) error {
 	buf := new(bytes.Buffer)
 	gz, _ := gzip.NewWriterLevel(buf, gzip.BestCompression)
-	gz.Name = buildName + "-" + ver
+	gz.Name = b.Name + "-" + b.ver
 	if _, err := io.Copy(gz, r); err != nil {
-		log.Fatal(err)
+		return err
 	}
 	if err := gz.Close(); err != nil {
-		log.Fatal(err)
+		return err
 	}
 
-	filename := buildName + "-" + ver + "-" + buildPlat + ".gz"
+	filename := b.Name + "-" + b.ver + "-" + b.OS + "-" + b.Arch + ".gz"
 	if err := s3put(buf, s3DistURL+filename); err != nil {
-		log.Fatal(err)
+		return err
 	}
-	return
-}
-
-func mustCmd(arg ...string) []byte {
-	output, err := cmd(arg...)
-	if err != nil {
-		log.Fatal(strings.Join(arg, " ")+": ", err)
-	}
-	return output
+	return nil
 }
 
 func cmd(arg ...string) ([]byte, error) {
@@ -158,48 +170,50 @@ func getCreds(u *url.URL) (user, pass string) {
 	return m.Login, m.Password
 }
 
-func mustRegister(ver string, sha256 []byte) {
-	url := distURL + buildName + "-" + ver + "-" + buildPlat + ".json"
-	b := new(bytes.Buffer)
-	err := json.NewEncoder(b).Encode(struct{ Sha256 []byte }{sha256})
+func (b *Build) register(sha256 []byte) error {
+	url := distURL + b.Name + "-" + b.ver + "-" + b.OS + "-" + b.Arch + ".json"
+	buf := new(bytes.Buffer)
+	err := json.NewEncoder(buf).Encode(struct{ Sha256 []byte }{sha256})
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
-	r, err := http.NewRequest("PUT", url, b)
+	r, err := http.NewRequest("PUT", url, buf)
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 	r.SetBasicAuth(getCreds(r.URL))
 	r.Header.Set("Date", time.Now().UTC().Format(http.TimeFormat))
 	resp, err := http.DefaultClient.Do(r)
 	if err != nil {
-		log.Fatal("error in mustRegister:", err)
+		return err
 	}
 	if resp.StatusCode != 201 {
 		body, _ := ioutil.ReadAll(resp.Body)
-		log.Fatalf("http status %v putting %q: %q", resp.Status, r.URL, string(body))
+		return fmt.Errorf("http status %v putting %q: %q", resp.Status, r.URL, string(body))
 	}
+	return nil
 }
 
-func mustSetCur(ver string) {
-	url := distURL + buildName + "-" + buildPlat + ".json"
-	b := new(bytes.Buffer)
-	err := json.NewEncoder(b).Encode(struct{ Version string }{ver})
+func (b *Build) setCurVersion() error {
+	url := distURL + b.Name + "-" + b.Platform() + ".json"
+	buf := new(bytes.Buffer)
+	err := json.NewEncoder(buf).Encode(struct{ Version string }{b.ver})
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
-	r, err := http.NewRequest("PUT", url, b)
+	r, err := http.NewRequest("PUT", url, buf)
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 	r.SetBasicAuth(getCreds(r.URL))
 	r.Header.Set("Date", time.Now().UTC().Format(http.TimeFormat))
 	resp, err := http.DefaultClient.Do(r)
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 	if resp.StatusCode != 200 {
 		body, _ := ioutil.ReadAll(resp.Body)
-		log.Fatalf("http status %v putting %q: %q", resp.Status, r.URL, string(body))
+		return fmt.Errorf("http status %v putting %q: %q", resp.Status, r.URL, string(body))
 	}
+	return nil
 }
--- hkdist/main.go
+++ hkdist/main.go
@@ -2,11 +2,12 @@
 //
 // It has three sub-commands: build, web, and gen.
 //
-//   $ hkdist build
+//   $ hkdist build [all|<platform> ...]
 //
-// This command fetches source code from github, builds it, uploads the
-// binary to and S3 bucket, and posts its SHA-256 hash to the hk distribution
-// server (hk.heroku.com in production).
+// This command builds cross-compiled binaries. For the current platform (or
+// all specified platforms), it first fetches source code from github, builds a
+// binary executable, uploads the binary to an S3 bucket, and posts its SHA-256
+// hash to the hk distribution server (hk.heroku.com in production).
 //
 //   $ hkdist web
 //
@@ -27,6 +28,8 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+	"runtime"
+	"strings"
 )
 
 var (
@@ -55,22 +58,85 @@ func (r release) Gzname() string {
 	return r.Name() + ".gz"
 }
 
-var subcmds = map[string]func(){
-	"gen":   gen,
-	"build": build,
-	"web":   web,
+func usage() {
+	fmt.Fprintln(os.Stderr, "Usage: hkdist (web|gen|build [all|<platform> ...])")
+	os.Exit(2)
+}
+
+var allPlatforms = []string{
+	"darwin-386",
+	"darwin-amd64",
+	"freebsd-386",
+	"freebsd-amd64",
+	"freebsd-arm",
+	"linux-386",
+	"linux-amd64",
+	"linux-arm",
+	"windows-386",
+	"windows-amd64",
 }
 
 func main() {
 	log.SetFlags(log.Lshortfile)
-	if len(os.Args) != 2 {
-		fmt.Fprintln(os.Stderr, "Usage: hkdist web|gen|build")
-		os.Exit(2)
+	if len(os.Args) < 2 {
+		usage()
+	} else if os.Args[1] != "build" && len(os.Args) != 2 {
+		usage()
 	}
-	f := subcmds[os.Args[1]]
-	if f == nil {
-		fmt.Fprintln(os.Stderr, "Usage: hkdist web|gen|build")
-		os.Exit(2)
+	switch os.Args[1] {
+	case "gen":
+		gen()
+	case "web":
+		web()
+	case "build":
+		mustHaveEnv("S3DISTURL")
+		mustHaveEnv("S3_ACCESS_KEY")
+		mustHaveEnv("S3_SECRET_KEY")
+		mustHaveEnv("DISTURL")
+		mustHaveEnv("BUILDBRANCH")
+		mustHaveEnv("BUILDNAME")
+
+		platforms := make([]string, 0)
+		if len(os.Args) > 2 {
+			if os.Args[2] == "all" {
+				platforms = allPlatforms
+			} else {
+				list := os.Args[2:]
+				for _, platform := range list {
+					if !isValidPlatform(platform) {
+						fmt.Fprintln(os.Stderr, "Allowed platforms:", strings.Join(allPlatforms, " "))
+						usage()
+					}
+				}
+				platforms = list
+			}
+		} else {
+			platforms = []string{runtime.GOOS + "-" + runtime.GOARCH}
+		}
+		// run Build for each platform
+		for _, platform := range platforms {
+			sepIndex := strings.Index(platform, "-")
+			b := &Build{
+				Repo:   "https://github.com/kr/hk.git",
+				Dir:    "hk",
+				Branch: branch,
+				Name:   buildName,
+				OS:     platform[:sepIndex],
+				Arch:   platform[sepIndex+1:],
+			}
+			err := b.Run()
+			if err != nil {
+				log.Printf("Error building %s on %s for %s-%s: %s", b.Name, b.Branch, b.OS, b.Arch, err)
+			}
+		}
 	}
-	f()
+}
+
+func isValidPlatform(platform string) bool {
+	for _, allowed := range allPlatforms {
+		if allowed == platform {
+			return true
+		}
+	}
+	return true
 }
